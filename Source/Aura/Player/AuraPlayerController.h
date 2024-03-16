001390ca76f1d071c31754a9ea0069686c1e497b
Aleksandar Ivic <125746113+Aleksdbz@users.noreply.github.com>
2024-03-16 22:27:43 +0100

    Aura Player Controller

--- Source/Aura/Player/AuraPlayerController.h
+++ Source/Aura/Player/AuraPlayerController.h
@@ -3,14 +3,33 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "EnhancedInputSubsystems.h"
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
 
+
+class UInputMappingContext;
 
 UCLASS()
 class AURA_API AAuraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+public:
+	AAuraPlayerController();
+
+
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputMappingContext> AuraContext;
+
+	
+
+	
+	
 	
 	
 };
